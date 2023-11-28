b2832f6cdfc24a0236d489192c58df532299fd1f
Omar G <Sama34@users.noreply.github.com>
2023-11-27 21:58:30 -0800

    Code cleanup

--- Upload/inc/languages/english/ougc_profileactivity.lang.php
+++ Upload/inc/languages/english/ougc_profileactivity.lang.php
@@ -3,29 +3,28 @@
 /***************************************************************************
  *
  *   OUGC Profile Activity plugin (/inc/languages/english/ougc_profileactivity.lang.php)
- *	 Author: Omar Gonzalez
+ *     Author: Omar Gonzalez
  *   Copyright: © 2012 Omar Gonzalez
- *   
+ *
  *   Website: http://community.mybb.com/user-25096.html
  *
  *   Show an overview of latest threads and/or posts in profile.
  *
  ***************************************************************************
- 
-****************************************************************************
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
+ ****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 
 $l['ougc_profileactivity_dateline'] = '{1} at {2}';
 $l['ougc_profileactivity_titlet'] = '{1}\'s Latest Threads';
